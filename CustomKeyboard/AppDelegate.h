4766608accb749c6ca03e7b7f0fb5766c178d029
Tom Bradley <bradth01@gmail.com>
2016-06-19 16:09:44 -0400

    Initial Commit

---
+++ CustomKeyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomKeyboard
+//
+//  Created by Thomas Bradley on 6/19/16.
+//  Copyright © 2016 Thomas Bradley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomKeyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomKeyboard
+//
+//  Created by Thomas Bradley on 6/19/16.
+//  Copyright © 2016 Thomas Bradley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
